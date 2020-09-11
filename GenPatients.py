f9ba473454a141df8fb87b4fefa1207dff46b2ca
MIGUEL CORES <micores@cisco.com>
2020-09-11 21:19:45 +0100

    edited GenPatients.py

--- GenPatients.py
+++ GenPatients.py
@@ -4,31 +4,33 @@ import json
 from Tools import PatientEmulator
 
 
+def save_patients_json(objeto, path, source, n_same_time):
+    if n_same_time:
+        file_name = 'emu-'+source+'_'+n_same_time+'.json'
+    else:
+        file_name = 'emu-'+source+'.json'
+    with open(os.path.join(path, file_name), 'w') as fp:
+        json.dump(objeto, fp, indent=2)
+    return
+
+
 def generate_patients(source='orpha', path='_emu', conds=100, patients_per_cond=3, lamb=1, ancestor_prob=0.5,
                       noise_ptg=0, n_same_time=None):
     emu = PatientEmulator(conds, patients_per_cond, lamb=lamb, ancestor_prob=ancestor_prob, noise_ptg=noise_ptg)
 
     if source == 'decipher' or source == 'all':
         decipher_patients = emu.emulate_conditions('DECIPHER')
-        with open(os.path.join(path, 'emu-decipher.json'), 'w') as fp:
-            json.dump(decipher_patients, fp, indent=2)
+        save_patients_json(decipher_patients, path, source, n_same_time)
 
     if source == 'orpha' or source == 'all':
         orpha_patients = emu.emulate_conditions('ORPHA')
-        if n_same_time:
-            with open(os.path.join(path, 'emu-orpha_'+n_same_time+'.json'), 'w') as fp:
-                json.dump(orpha_patients, fp, indent=2)
-        else:
-            with open(os.path.join(path, 'emu-orpha.json'), 'w') as fp:
-                json.dump(orpha_patients, fp, indent=2)
+        save_patients_json(orpha_patients, path, source, n_same_time)
 
     if source == 'omim' or source == 'all':
         omim_patients = emu.emulate_conditions('OMIM')
-        with open(os.path.join(path, 'emu-omim.json'), 'w') as fp:
-            json.dump(omim_patients, fp, indent=2)
+        save_patients_json(omim_patients, path, source, n_same_time)
 
     if source == 'all':
         decipher_patients.update(orpha_patients)
         decipher_patients.update(omim_patients)
-        with open(os.path.join(path, 'emu-all.json'), 'w') as fp:
-            json.dump(decipher_patients, fp, indent=2)
+        save_patients_json(decipher_patients, path, source, n_same_time)
